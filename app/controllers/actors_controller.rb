61291baac46a8b7c34ec321fe3bb02f3c639e57d
nehir-mentes <nehirmen.98@gmail.com>
2025-04-29 04:16:37 +0000

    actors_working

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -7,17 +7,8 @@ class ActorsController < ApplicationController
     the_id = params.fetch("the_id")
     matching_records = Actor.where({ :id => the_id })
     @the_actor = matching_records.at(0)
-    
-    matching_character = Character.where({ :actor_id => the_id })
-    @the_movie = matching_character
-    #.at(0).movie_id
 
-    render({ :template => "actor_templates/sample"})
-
-    #matching_director = Movie.where({ :id => @the_movie })
-    #@the_director = matching_director.at(0).name
-
-    #render({ :template => "actor_templates/actor_details"})
+    render({ :template => "actor_templates/actor_details"})
   end
 
 end
